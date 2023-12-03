4a8c8c412759afdcdf160ad0628fe2b4b7136656
Lewis Boon <4803529+cantoboon@users.noreply.github.com>
2023-12-03 18:27:04 +0000

    Fixing build

--- app/page.tsx
+++ app/page.tsx
@@ -12,9 +12,9 @@ function UserProfile() {
   return (
     user && (
       <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+        {/* <img src={user?.picture} alt={user?.name} /> */}
+        <h2>{user?.name}</h2>
+        <p>{user?.email}</p>
       </div>
     )
   );
